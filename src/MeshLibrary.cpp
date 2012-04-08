a1ba8ce808393bf223cd0db58d581aa66691a1d5
Sean Lilley <sebby_man@yahoo.com>
2012-04-08 19:47:16 -0400

    added primitive shapes

--- src/MeshLibrary.cpp
+++ src/MeshLibrary.cpp
@@ -12,11 +12,24 @@ MeshLibrary::~MeshLibrary()
 
 void MeshLibrary::initialize()
 {
+	ColladaData* data;
+	std::string filename;
 
-	std::string filename = "data/meshes/pumpkin.dae";
-	ColladaData* data = Loader::readColladaAsset(Utils::getFilePath(filename));
-	Mesh* mesh = new Mesh(data->positionData, data->normalsData, data->elementArray);
-	meshes.push_back(mesh);
+	filename = "data/meshes/pumpkin.dae";
+	data = Loader::readColladaAsset(Utils::getFilePath(filename));
+	meshes.push_back(new Mesh(data->positionData, data->normalsData, data->elementArray));
+
+	filename = "data/meshes/cube.dae";
+	data = Loader::readColladaAsset(Utils::getFilePath(filename));
+	meshes.push_back(new Mesh(data->positionData, data->normalsData, data->elementArray));
+
+	filename = "data/meshes/cylinder.dae";
+	data = Loader::readColladaAsset(Utils::getFilePath(filename));
+	meshes.push_back(new Mesh(data->positionData, data->normalsData, data->elementArray));
+
+	filename = "data/meshes/sphere.dae";
+	data = Loader::readColladaAsset(Utils::getFilePath(filename));
+	meshes.push_back(new Mesh(data->positionData, data->normalsData, data->elementArray));
 }
 
 
--- src/MeshLibrary.h
+++ src/MeshLibrary.h
@@ -19,6 +19,4 @@ public:
 
 	Mesh* getMesh(int number);
 
-
-
 };
